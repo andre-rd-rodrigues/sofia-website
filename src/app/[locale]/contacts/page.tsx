3101db0992e0e583986cf8ed374ec1c44c8cc7f2
Andre Rodrigo <andre.rodrigues@probely.com>
2025-05-12 10:56:08 +0100

    add contacts

---
+++ src/app/[locale]/contacts/page.tsx
@@ -0,0 +1,21 @@
+import Page from "@/components/Page";
+
+import { useTranslations } from "next-intl";
+
+import Contacts from "@/components/Contacts";
+
+const ContactsPage = () => {
+  const t = useTranslations("pages.contacts");
+
+  return (
+    <Page>
+      <Page.Title
+        src="https://askproject.net/medral/wp-content/uploads/sites/154/2023/09/small-white-ceramic-mortar-with-eucalyptus-leaves-AANHPA2.jpg"
+        title={t("subtitle")}
+      />
+      <Contacts />
+    </Page>
+  );
+};
+
+export default ContactsPage;
